9eab93ccd924e1dff81fbe03a93ba1094367f84b
Ethan Liu <liuyuxuan1230@outlook.com>
2020-05-10 18:40:07 +0800

    40. extract handleChange, arrow function & this scope

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios'
-import { CardList } from './components/card-list/card-list.component';
+import React, { Component } from 'react'
+import './App.css'
+import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor () {
+    super()
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
     }
   }
-
 
   componentDidMount () {
     //1.
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(users => this.setState({ monsters: users }))
+      .then((res) => res.json())
+      .then((users) => this.setState({ monsters: users }))
 
     // // 2. using axios
     // axios.get('https://jsonplaceholder.typicode.com/users')
@@ -26,14 +25,22 @@ class App extends Component {
     //   .then(({ data }) => this.setState({ monsters: data }))
   }
 
+  handleChange = e => this.setState({ searchField: e.target.value })
+
   render () {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()),
+    )
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters}>
-        </CardList>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange}/>
+        <CardList monsters={filteredMonsters}/>
       </div>
     )
   }
 }
 
-export default App;
+export default App
