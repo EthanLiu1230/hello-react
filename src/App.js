1029235f89323f80c249ee4de167eab98b511b8a
Ethan Liu <liuyuxuan1230@outlook.com>
2020-05-10 09:17:05 +0800

    26. Fetching Content

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    }
+  }
+
+
+  componentDidMount () {
+    //1.
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState({ monsters: users }))
+
+    // // 2. using axios
+    // axios.get('https://jsonplaceholder.typicode.com/users')
+    //   // .then(res => res.data)
+    //   .then(({ data }) => this.setState({ monsters: data }))
+  }
+
+  render () {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(
+            monster => <h1>{monster.name}</h1>
+          )
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
