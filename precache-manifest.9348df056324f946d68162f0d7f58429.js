b0b82f5af96ba0ddf51b587cfea06601e94a21a3
Ethan Liu <liuyuxuan1230@outlook.com>
2020-05-10 19:09:35 +0800

    Updates

---
+++ precache-manifest.9348df056324f946d68162f0d7f58429.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e87a0f6a81f14607336e8b30e0124e77",
+    "url": "/hello-react/index.html"
+  },
+  {
+    "revision": "ec7872663794d6aee02a",
+    "url": "/hello-react/static/css/main.e05f77e5.chunk.css"
+  },
+  {
+    "revision": "14f0028c6f9296e231c4",
+    "url": "/hello-react/static/js/2.09e571a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-react/static/js/2.09e571a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7872663794d6aee02a",
+    "url": "/hello-react/static/js/main.38eadf3c.chunk.js"
+  },
+  {
+    "revision": "81ec208232df873545bc",
+    "url": "/hello-react/static/js/runtime-main.c4d18ffe.js"
+  }
+]);
